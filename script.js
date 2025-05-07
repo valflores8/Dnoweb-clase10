5a8266b78ad3f17e5381852103052ec6f46e239f
valflores8 <87672674+valflores8@users.noreply.github.com>
2025-05-07 11:44:31 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,25 @@
-
 const filas = document.querySelector("tbody");
 const texto = document.querySelector("#promedio");
 var total = 0;
 function carita(nota) {
     var emoji;
-      if (nota > 5.9) {
-      	emoji = "😃";
+    if (nota > 5.9) {
+        emoji = "😃";
     } else if (nota == 5.9) {
-    	emoji = "😐";
+        emoji = "😐";
     } else if (nota <= 4) {
-    	emoji = "😭";
+        emoji = "😭";
     } else {
-    	emoji = "🙁";
+        emoji = "🙁";
     }
     return emoji;
-    }
+}
 // if (nota > 5.9) {  return "😃" } else
 
-function barrita(n){  let width= n*40;  return width;   }
+function barrita(n) {
+    let width = n * 40;
+    return width;
+}
 // viewbox {0 0 210 30}
 
 async function notas() {
@@ -25,8 +27,7 @@ async function notas() {
     let data = await consulta.json();
     console.log(data);
     data.forEach((d) => {
-        filas.innerHTML += 
-        `<tr> 
+        filas.innerHTML += `<tr> 
         <td>${carita(d.nota)}</td>
         <td>${d.nombre}</td> 
         <td>
@@ -34,9 +35,9 @@ async function notas() {
             <rect x="0" y="0" width="280" height="30" fill="lightyellow"> </rect>
             <rect x="0" y="0" width="${barrita(d.nota)}" height="30" fill="#5b1868"> </rect>
                 <text x="10%" y="70%" fill="white" font-weight="700">
-                ${(d.nota).toFixed(1)} </text>
-            <line x1="${5.9*40}" y1="0" x2="${5.9*40}" y2="15" stroke="#ffa000" stroke-width="2"></line>
-            <line x1="${4*40}" y1="0" x2="${4*40}" y2="15" stroke="#dd2c00" stroke-width="2"></line>
+                ${d.nota.toFixed(1)} </text>
+            <line x1="${5.9 * 40}" y1="0" x2="${5.9 * 40}" y2="15" stroke="#ffa000" stroke-width="2"></line>
+            <line x1="${4 * 40}" y1="0" x2="${4 * 40}" y2="15" stroke="#dd2c00" stroke-width="2"></line>
             </svg> 
         </td>
         </tr> `;
@@ -44,9 +45,9 @@ async function notas() {
         total = total + d.nota;
     });
     texto.innerHTML = (total / 12).toFixed(1);
-    }
-    
-//  <td> ${(d.nota).toFixed(1)} </td>  
+}
+
+//  <td> ${(d.nota).toFixed(1)} </td>
 
 notas().catch((error) => console.error(error));
 
